0282c82e1f00bdc6967b373e4392771a34e96763
laizerong <laizerong@51yryc.com>
2020-05-14 10:42:42 +0800

    按需引入vant组件

--- src/main.js
+++ src/main.js
@@ -12,11 +12,33 @@ axios.defaults.baseURL = "http://mockjs.com/api"; // 设置默认请求的url
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
-import { Lazyload } from "vant";
+import { 
+  Lazyload,
+  Image,
+  NavBar,
+  Empty,
+  PullRefresh,
+  List,
+  Icon,
+  GoodsAction,
+  GoodsActionIcon,
+  GoodsActionButton
+} from "vant";
+
 Vue.use(Lazyload, {
   preLoad: 1.3,
   attempt: 3
 });
+Vue.use(Image)
+.use(NavBar)
+.use(Empty)
+.use(PullRefresh)
+.use(List)
+.use(Icon)
+.use(GoodsAction)
+.use(GoodsActionIcon)
+.use(GoodsActionButton);
+
 new Vue({
   router,
   store,
